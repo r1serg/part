d96ab1cc7d2d281f078119c997063ce878bd33da
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-01 18:32:57 +0300

    commit

--- part.py
+++ part.py
@@ -3,4 +3,19 @@ a=int(input())
 b=int(input())
 c=(a**2+b**2)**0.5
 print(c)
-2**8
+g=int(input())
+v=g+1
+print("The next number for the number", g, "is", v)
+v=g-1
+print("The next number for the number", g, "is", v)
+u=int(input())
+u=u%10
+print(u)
+h=int(input())
+h=h%100
+h=h//10
+print(h)
+s=input()
+print(s)
+
+
